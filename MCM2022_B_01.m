f858152b8e28e08aed59197e72523d4291770c31
lxy <lxy11132@qq.com>
2025-01-22 20:11:21 +0800

    third commit

--- MCM2022_B_01.m
+++ MCM2022_B_01.m
@@ -1,11 +1,11 @@
 alpha = zeros(2,5);
 gama = zeros(2,5);
-inflow = 0;
+inflow = 27812642;
 Square = [658*(10^6) 640*(10^6)];% 面积(Glen Canyon Dam, Hoover Dam)
 beta = zeros(1,2);
 density = 1;
 gravity = 9.7979;
-height_lowest = [110, 119];
+height_lowest = [110, 119; 65, 79];
 
 [v, u, height, t1, t2] = water_allocation_one(alpha, gama, inflow, Square, beta, density, gravity, height_lowest);
 disp("height");
--- water_allocation_one.m
+++ water_allocation_one.m
@@ -2,23 +2,26 @@ function [v, u, height, t1, t2] = water_allocation_one(alpha, gama, inflow, Squa
 t = 0;
 mark = 0;
 height = [142 158]; %水位高度 (Glen Canyon Dam, Hoover Dam)
+hold on
+scatter(t, height);
+
 Volume = zeros(1,2);
 Volume(1) = height(1) * Square(1);
 Volume(2) = height(2) * Square(2);
 disp("Volume2");
 disp(Volume);
 
-while(height(1) > height_lowest(1) || height(2) > height_lowest(2))
+while(height(1) > height_lowest(1,1) || height(2) > height_lowest(2))
     t = t + 1;
     if(height(1) > height_lowest(1) && height(2) > height_lowest(2))
-        v = [2.38*(10^8), 1.12*(10^9), 1.23*(10^8), 4.45*(10^8), 0; 0, 0, 0, 0, 4.42*(10^8)];
+        v = [0, 0, 0, 0, 4.42*(10^8); 2.38*(10^8), 1.12*(10^9), 1.23*(10^8), 4.45*(10^8), 0];
         u = [8625*(0.3048^3)*3600*24, 3.51*(10^7)];
     elseif(height(1) <= height_lowest(1) && height(2) > height_lowest(2))
-        v = [10000000, 10000000, 10000000, 10000000, 10000000; 10000000, 10000000, 10000000, 10000000, 10000000];
+        v = [0, 0, 0, 0, 4.42*(10^8); 2.38*(10^8), 1.12*(10^9), 1.23*(10^8), 4.45*(10^8), 0];
         u = [8625*(0.3048^3)*3600*24, 3.51*(10^7)];
         v(1, :) = 0;
     elseif(height(1) > height_lowest(1) && height(2) <= height_lowest(2))
-        v = [10000000, 10000000, 10000000, 10000000, 10000000; 10000000, 10000000, 10000000, 10000000, 10000000];
+        v = [0, 0, 0, 0, 4.42*(10^8); 2.38*(10^8), 1.12*(10^9), 1.23*(10^8), 4.45*(10^8), 0];
         u = [8625*(0.3048^3)*3600*24, 3.51*(10^7)];
         v(2, :) = 0;
     else
@@ -35,6 +38,8 @@ while(height(1) > height_lowest(1) || height(2) > height_lowest(2))
     height = Volume ./ Square;
     disp(t);
     disp(height);
+    hold on
+    scatter(t, height);
 end
 
 t2 = t;
